309de6c76ce113ac08b871ffb343427ab7bfc6b8
Denys Lunhu <den1k.vps2@gmail.com>
2020-09-20 12:23:22 +0300

    Add sort functions

--- script.js
+++ script.js
@@ -1,43 +1,79 @@
 "use strict";
 
 let main = document.querySelector(".main");
+
 let input = document.querySelector(".file-input");
+let select = document.querySelector(".file-sort-select");
 
-input.addEventListener("change", function(){
-	readFile(this);
-});
+document.addEventListener("keyup", function(e){
 
-function readFile(input) {
-  let file = input.files[0];
+	if(e.code == "Enter"){
 
-  let reader = new FileReader();
+		document.querySelector(".words-table") ? document.querySelector(".words-table").remove() : false;
+		readFile(input, select.value);
 
-  reader.readAsText(file);
+	}
+	
+})
 
-  reader.onload = function() {
-  	buildTableByMap(getUniqWords(reader.result));
-  };
+document.onkeydown = function() {
 
-  reader.onerror = function() {
-    document.body.append(reader.error);
-  };
+	input.blur();
+	select.blur();
+
+}
+
+function readFile(input, sort_type) {
+
+	let file = input.files[0];
+
+	let reader = new FileReader();
+
+	reader.readAsText(file);
+
+	reader.onload = function() {
+
+		let start_time = Date.now();
+
+		let sort_uniq_words = undefined;
+		let result = reader.result;
+
+		switch(sort_type){
+
+			case "1":
+				sort_uniq_words = sortMapByAscending(getUniqWords(result));
+				break;
+			case "2":
+				sort_uniq_words = sortMapByDescending(getUniqWords(result));
+				break;
+			case "3":
+				sort_uniq_words = sortMapByAlphabetical(getUniqWords(result));
+				break;
+
+		}
+
+		buildTableByMap(sort_uniq_words);
+
+		console.log(`Executing time is ${(Date.now() - start_time) /1000} sec.`);
+		  
+	};
+
+	reader.onerror = function() {
+		  
+		alert(new Error("Error loading file"));
+
+	};
 
 }
 
 function getUniqWords(result){
 
-	let words = result.replace(/[!?:;,."]/g, "").toLowerCase().trim().split(" ");
+	let words = result.replace(/[!?:;,.\-"]/g, "").toLowerCase().split(" ");
 
 	let uniq_words = new Map();
 
-	words.forEach(function(item){
-
-		if(!uniq_words.has(item))
-			uniq_words.set(item, 1);
-		else
-			uniq_words.set(item, uniq_words.get(item) + 1);
-
-	});
+	words.forEach((item) => uniq_words.has(item) ? uniq_words.set(item, uniq_words.get(item) + 1) : uniq_words.set(item, 1));
+	uniq_words.delete("");
 
 	return uniq_words;
 
@@ -69,5 +105,39 @@ function buildTableByMap(map){
 	}
 
 	main.append(table);
+
+}
+
+function sortMapByAscending(map){
+
+	let arr = Object.entries(Object.fromEntries(map));
+	
+	arr.sort(function(a, b){
+		return a[1] - b[1];
+	});
+
+	return new Map(arr);
+
+}
+
+function sortMapByDescending(map){
+
+	let arr = Object.entries(Object.fromEntries(map));
+	
+	arr.sort(function(a, b){
+		return b[1] - a[1];
+	});
+
+	return new Map(arr);
+
+}
+
+function sortMapByAlphabetical(map){
+
+	let arr = Object.entries(Object.fromEntries(map));
+	
+	arr.sort();
+
+	return new Map(arr);
 
 }
